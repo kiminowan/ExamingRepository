d4dbcde96ef5dfa9baf8de566b1be334e7cffb46
2395073949 <2395073949@qq.com>
2018-12-05 10:25:50 +0800

    知识点类型实体

---
+++ QualificationExaming/QualificationExaming.Entity/KnowledgePoint.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QualificationExaming.Entity
+{
+    /// <summary>
+    /// 知识点类型表
+    /// </summary>
+    public class KnowledgePoint
+    {
+        /// <summary>
+        /// 知识点ID
+        /// </summary>
+        public int KnowledgePointID { get; set; }
+        /// <summary>
+        /// 知识点名称
+        /// </summary>
+        public string KnowledgePointName { get; set; }
+    }
+}
