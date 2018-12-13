d53a6f9f3b0052c802be6ce0e6bfd166192011c2
kiminowan <422149417@qq.com>
2018-12-13 13:29:00 +0800

    后台页面1213

---
+++ QualificationExaming/QualificationExaming.UI/Controllers/DefaultController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace QualificationExaming.UI.Controllers
+{
+    public class DefaultController : Controller
+    {
+        // GET: Default
+        public JsonResult Index()
+        {
+            var file = System.Web.HttpContext.Current.Request.Files;
+            var name = file[0].FileName;
+            return Json(file[0].FileName);
+        }
+    }
+}
