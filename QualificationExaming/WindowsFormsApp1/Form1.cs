2df3162ff1ea97824c98561bda1ec275befe2f9b
2395073949 <2395073949@qq.com>
2018-12-25 20:02:46 +0800

    QYG

---
+++ QualificationExaming/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.OleDb;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private DataTable dt;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                //提示打开文件
+                OpenFileDialog file = new OpenFileDialog();
+                //筛选文件格式
+                file.Filter = "Excel(*.xls)|*.xls|Excel(*.xlsx)|*.xlsx";
+                //如果点击取消就返回
+                if (file.ShowDialog() == DialogResult.Cancel)
+                {
+                    return;
+                }
+                //获得所选文件名
+                string fileName = file.FileName;
+                //把Excel当做数据源连接
+                String sConnectionString = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source=" + fileName + ";Extended Properties=Excel 12.0;";
+                //打开Excel数据源
+                OleDbConnection conn = new OleDbConnection(sConnectionString);
+                conn.Open();
+                //查询Excel表语句
+                string sql = "select * from [Sheet1$]";
+                //执行sql语句
+                OleDbCommand cmd = new OleDbCommand(sql, conn);
+                //打开适配器
+                OleDbDataAdapter adapter = new OleDbDataAdapter();
+                //获得的数据放到适配器
+                adapter.SelectCommand = cmd;
+                //定义DataSet
+                DataSet ds = new DataSet();
+                //数据放到DataSet
+                adapter.Fill(ds);
+                //获得DataSet第一个表dataTable
+                dt = ds.Tables[0];
+                //数据显示到dataGridView1
+                this.dataGridView1.DataSource = dt;
+                //循环表
+                for (int i = 0; i < dt.Rows.Count; i++)
+                {
+                    //获得添加的sql语句 并执行
+                    string stuSQL = string.Format("insert into question(QuestionName, ChoiceA, ChoiceB, ChoiceC, ChoiceD,Answer,KnowledgePointID,Analysis,QuestionHasImg,QuestionImg,ChoiceIsImg,ExamID,TypeID) values('{0}','{1}','{2}','{3}','{4}','{5}','{6}','{7}','{8}','{9}','{10}','{11}','{12}')", dt.Rows[i]["题干"], dt.Rows[i]["A选项(选项若为图片则写图片名称带扩展名)"], dt.Rows[i]["B选项"], dt.Rows[i]["C选项"], dt.Rows[i]["D选项"], dt.Rows[i]["答案（多选题为ABCD的组合，判断题A为对B为错）"], dt.Rows[i]["知识点ID"], dt.Rows[i]["解析"], dt.Rows[i]["题干是否有图片(1为有，0为没有)"], dt.Rows[i]["题干图片名称带扩展名(没有则留空)"], dt.Rows[i]["选项是否为图片(1为有，0为没有)"], dt.Rows[i]["所属考卷ID"], dt.Rows[i]["类型（单选、多选、判断）"]);
+                    DBhelp.AddOrDelteOrUpdate(stuSQL, CommandType.Text);
+                }
+                //关闭数据源
+                conn.Close();
+                //提示
+                MessageBox.Show("导入成功！");
+            }
+            //抛出异常
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
---
+++ QualificationExaming/WindowsFormsApp1/MySQLDBhelp.cs
@@ -0,0 +1,262 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using MySql.Data.MySqlClient;
+using System.Data;
+using System.Configuration;
+using System.Collections;
+public class DBhelp
+{
+    //获得数据库连接字符串
+    //private static readonly string ConnectionString = ConfigurationManager.ConnectionStrings["StudentDB"].ConnectionString;
+    private static string ConnectionString = "server=172.25.53.26;user id=root;password=fense0415sx;database=db_examing;sslmode=none;";
+    /// <summary>
+    /// sql语句查询返回datatable
+    /// </summary>
+    /// <param name="sql"></param>
+    /// <returns></returns>
+    public static DataTable GetTabel(string sql, CommandType commandType, params MySqlParameter[] parameters)
+    {
+        MySqlConnection conn = new MySqlConnection(ConnectionString);
+        DataTable dt = new DataTable();
+        try
+        {
+            conn.Open();
+            MySqlDataAdapter adapter = new MySqlDataAdapter(sql, conn);
+            adapter.SelectCommand.CommandType = commandType;
+            adapter.SelectCommand.Parameters.AddRange(parameters);
+            adapter.Fill(dt);
+        }
+        catch (Exception ex)
+        {
+            dt = null;
+            throw ex;
+        }
+        finally
+        {
+            conn.Close();
+            conn.Dispose();
+            conn = null;
+        }
+        return dt;
+    }
+    /// <summary>
+    /// 执行sql语句添加 删除 修改
+    /// </summary>
+    /// <param name="sql"></param>
+    /// <returns></returns>
+    public static int AddOrDelteOrUpdate(string sql, CommandType commandType, params MySqlParameter[] parameters)
+    {
+        MySqlConnection conn = new MySqlConnection(ConnectionString);
+        try
+        {
+            conn.Open();
+        }
+        catch (MySqlException ex)
+        {
+            // 获取描述当前异常的消息。
+            // 返回结果:解释异常原因的错误消息或空字符串 ("")。
+            throw new Exception(ex.Message);
+        }
+        MySqlCommand cmd = new MySqlCommand(sql, conn);
+        cmd.CommandType = commandType;
+        cmd.Parameters.AddRange(parameters);
+        try
+        {
+            cmd.Prepare();
+            int i = cmd.ExecuteNonQuery();
+            return i;
+        }
+        catch (Exception ex)
+        {
+            throw (ex);
+        }
+        finally
+        {
+            conn.Close();
+            conn.Dispose();
+            conn = null;
+            cmd.Dispose();
+            cmd = null;
+        }
+    }
+
+    /// <summary>
+    /// 执行批删 批修
+    /// </summary>
+    /// <param name="sqlList"></param>
+    public static void AllDelteOrAllUpdate(ArrayList sqlList)
+    {
+        using (MySqlConnection conn = new MySqlConnection(ConnectionString))
+        {
+            conn.Open();
+            MySqlCommand cmd = new MySqlCommand();
+            cmd.Connection = conn;
+            MySqlTransaction trans = conn.BeginTransaction();
+            cmd.Transaction = trans;
+            try
+            {
+                for (int i = 0; i < sqlList.Count; i++)
+                {
+                    string sql = sqlList[i].ToString();
+                    if (sql.Trim().Length > 1)
+                    {
+                        cmd.CommandText = sql;
+                        cmd.ExecuteNonQuery();
+                    }
+                }
+                trans.Commit();
+            }
+            catch (Exception ex)
+            {
+                trans.Rollback();
+                throw ex;
+            }
+            finally
+            {
+                conn.Close();
+                cmd.Dispose();
+                trans.Dispose();
+            }
+        }
+    }
+
+    /// <summary>
+    /// 登陆
+    /// </summary>
+    /// <param name="sql"></param>
+    /// <returns></returns>
+    public static object Login(string sql)
+    {
+        MySqlConnection conn = new MySqlConnection(ConnectionString);
+
+        try
+        {
+            conn.Open();
+        }
+        catch (Exception ex)
+        {
+            throw new Exception(ex.Message);
+        }
+        MySqlCommand cmd = new MySqlCommand(sql, conn);
+        try
+        {
+            cmd.Prepare();
+            return cmd.ExecuteScalar();
+        }
+        catch (Exception ex)
+        {
+            throw (ex);
+        }
+        finally
+        {
+            conn.Close();
+            conn.Dispose();
+            conn = null;
+            cmd.Dispose();
+            cmd = null;
+        }
+    }
+
+    /// <summary>
+    /// 分页存储过程
+    /// </summary>
+    /// <param name="sql">sql语句</param>
+    /// <param name="pageIndex">当前页</param>
+    /// <param name="pageSize">每页几条数据</param>
+    /// <param name="dataCount">总数据条数</param>
+    /// <returns></returns>
+    //public static DataTable FenYen(string sql,int pageIndex,int pageSize,out int dataCount)
+    //{
+    //    MySqlConnection conn = new MySqlConnection(ConnectionString);
+
+    //    try
+    //    {
+    //        conn.Open();
+    //    }
+    //    catch(Exception ex)
+    //    {
+    //        throw new Exception(ex.Message);
+    //    }
+    //    try
+    //    {
+    //        MySqlCommand cmd = new MySqlCommand();
+    //        cmd.CommandText = "";
+    //        cmd.CommandType = CommandType.StoredProcedure;
+    //        cmd.Connection = conn;
+    //        cmd.Parameters.Clear();
+    //        MySqlParameter[] parameter = new MySqlParameter[]
+    //        {
+    //            new MySqlParameter("",sql),
+    //            new MySqlParameter("",pageIndex),
+    //            new MySqlParameter("",pageSize),
+    //            new MySqlParameter("",MySqlDbType.Int32),
+    //        };
+    //        parameter[3].Direction = ParameterDirection.Output;
+
+    //        cmd.Parameters.Add(parameter);
+
+    //        MySqlDataAdapter adapter = new MySqlDataAdapter(cmd);
+    //        DataTable dt = new DataTable();
+    //        adapter.Fill(dt);
+    //        dataCount = Convert.ToInt32(parameter[3].Value);
+    //        adapter.Dispose();
+    //        return dt;
+    //    }
+    //    catch(Exception ex)
+    //    {
+    //        throw (ex);
+    //    }
+    //    finally
+    //    {
+    //        conn.Close();
+    //        conn.Dispose();
+    //        conn = null;
+    //    }
+
+    //}
+
+    /// <summary>
+    /// 存储过程 添加 删除  修改
+    /// </summary>
+    /// <returns></returns>
+    //public static int ProcAddOrDelteOrUpdate(string m)
+    //{
+    //    MySqlConnection conn = new MySqlConnection(ConnectionString);
+
+    //    try
+    //    {
+    //        conn.Open();
+    //    }
+    //    catch(Exception ex)
+    //    {
+    //        throw new Exception(ex.Message);
+    //    }
+    //    try
+    //    {
+    //        MySqlCommand cmd = new MySqlCommand();
+    //        cmd.CommandText = "";
+    //        cmd.CommandType = CommandType.StoredProcedure;
+    //        cmd.Connection = conn;
+
+    //        cmd.Parameters.Clear();
+
+    //        cmd.Parameters.AddRange(parameter);
+    //        return cmd.ExecuteNonQuery();
+    //    }
+    //    catch(Exception ex)
+    //    {
+    //        throw (ex);
+    //    }
+    //    finally
+    //    {
+    //        conn.Close();
+    //        conn.Dispose();
+    //        conn = null;
+    //    }
+    //}
+
+}
