0d45eb0f0190b70387ee413068b4bc766c7fab52
2395073949 <2395073949@qq.com>
2018-12-25 14:13:42 +0800

    QYG

--- QualificationExaming/QualificationExaming.UI/Controllers/DefaultController.cs
+++ QualificationExaming/QualificationExaming.UI/Controllers/DefaultController.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using Clock.DAL;
+using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.OleDb;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -15,6 +18,45 @@ namespace QualificationExaming.UI.Controllers
             var name = file[0].FileName;
             return Json(file[0].FileName);
         }
-        
+        public string GetExcel()
+        {
+            //获取所选文件
+            HttpPostedFileBase getFile = Request.Files["Excel"];
+            if (getFile != null)
+            {
+                //获得所选文件名
+                string fileName = Server.MapPath("~/Content/") + getFile.FileName;
+                if (!System.IO.File.Exists(fileName))
+                    getFile.SaveAs(fileName);
+                //把Excel当做数据源连接
+                string connStr = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source=" + fileName + ";Extended Properties=Excel 12.0;";
+                //打开Excel
+                OleDbConnection conn = new OleDbConnection(connStr);
+                conn.Open();
+                //查询Excel
+                string sql = "select * from [Sheet1$]";
+                OleDbCommand cmd = new OleDbCommand(sql, conn);
+                //初始化适配器
+                OleDbDataAdapter adapter = new OleDbDataAdapter();
+                //获取查出来的Excel表
+                adapter.SelectCommand = cmd;
+                //初始化dataset并通过适配器赋值
+                DataSet ds = new DataSet();
+                adapter.Fill(ds);
+                //获得ds的第一个表并添加到dataGridView1显示
+                DataTable dt = ds.Tables[0];
+                for (int i = 0; i < dt.Rows.Count; i++)
+                {
+                    string sqlStr = string.Format("insert into UsersClock(Department, Name, Num, ClockDate, position) values('{0}','{1}','{2}','{3}','{4}')", dt.Rows[i]["Department"], dt.Rows[i]["Name"], dt.Rows[i]["Num"], dt.Rows[i]["ClockDate"], dt.Rows[i]["position"]);
+                    DBhelper.AddOrDeleteOrUpdate(sqlStr, CommandType.Text);
+                }
+                conn.Close();
+                return "导入成功！";
+            }
+            else
+            {
+                return "导入失败！";
+            }
+        }
     }
 }
---
+++ QualificationExaming/QualificationExaming.UI/DBhelper.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clock.DAL
+{
+    using System.Data;
+    using System.Data.SqlClient;
+    using System.Configuration;
+    public class DBhelper
+    {
+        //获得数据库连接字符串
+        private static readonly string ConnectionString = ConfigurationManager.ConnectionStrings["Exam04StudentDB"].ConnectionString;
+        /// <summary>
+        /// 执行SQL语句
+        /// </summary>
+        /// <param name="sql"></param>
+        /// <param name="commType">命令类型</param>
+        /// <param name="parmeter">sql动态参数</param>
+        /// <returns></returns>
+        private static SqlCommand ExecuteCommand(string sql, SqlConnection conn, CommandType commType, params SqlParameter[] parmeter)
+        {
+            SqlCommand cmd = new SqlCommand(sql, conn);
+            cmd.CommandType = commType;
+            if (parmeter != null)
+            {
+                cmd.Parameters.AddRange(parmeter);
+            }
+            return cmd;
+        }
+        /// <summary>
+        /// 获取DataTable
+        /// </summary>
+        /// <param name="sql"></param>
+        /// <param name="commType"></param>
+        /// <param name="parameter"></param>
+        /// <returns></returns>
+        public static DataTable GetTable(string sql, CommandType commType, params SqlParameter[] parameter)
+        {
+            using (SqlConnection conn = new SqlConnection(ConnectionString))
+            {
+                using (SqlCommand cmd = ExecuteCommand(sql, conn, commType, parameter))
+                {
+                    SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+                    DataTable dt = new DataTable();
+                    adapter.Fill(dt);
+                    return dt;
+                }
+            }
+        }
+        /// <summary>
+        /// 添加
+        /// </summary>
+        /// <param name="sql"></param>
+        /// <param name="commType"></param>
+        /// <param name="parameter"></param>
+        /// <returns></returns>
+        public static int AddOrDeleteOrUpdate(string sql, CommandType commType, params SqlParameter[] parameter)
+        {
+            using (SqlConnection conn = new SqlConnection(ConnectionString))
+            {
+                using (SqlCommand cmd = ExecuteCommand(sql, conn, commType, parameter))
+                {
+                    conn.Open();
+                    int i = cmd.ExecuteNonQuery();
+                    return i;
+                }
+            }
+        }
+    }
+}
